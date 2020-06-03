c616f2a6c68ddb11253050f9f4762e0870e7c8a5
john demlon <53253324+johndemlon@users.noreply.github.com>
2020-06-03 07:02:10 +0300

    Add files via upload

---
+++ 24Solver.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+class Arith(object):
+ def __init__(self,a,b):
+  self.a = max(a,b)
+  self.b = min(a,b)
+
+ def add(self):
+  return self.a+self.b
+
+ def sub(self):
+  return self.a-self.b
+
+ def mul(self):
+  return self.a*self.b
+
+ def div(self):
+  try:
+   n=round(self.a/eval(str('{}.0'.format(self.b))),3)
+   return self.a/self.b if '.0' in str(n) else n
+  except:return 0
+
+ def combs(self):
+  arith=[self.add(),self.sub(),self.mul(),self.div()]
+  return arith
+
+class Solution(object):
+ def __init__(self,lst):
+  self.list = lst
+  self.mem  = []
+
+ def solve(self):
+  for a,alpha in enumerate(self.list):
+   for b,delta in enumerate(self.list): # Stage 1  [3,1,2,2] --> [1,2,3]
+    if a==b:continue
+    _=[_ for _ in range(4) if all([_!=a,_!=b])]
+    gamma=self.list[_[0]]
+    theta=self.list[_[1]]
+
+    # method 1
+    for num1,comb1 in enumerate(Arith(alpha,delta).combs()):
+     if num1:del self.mem[:]
+     # rearrange the order of alpha & delta - instead of 1-2=1 make it 2-1=1
+     alpha1=alpha if alpha>=delta else delta
+     delta1=alpha if alpha1==delta else delta
+
+     # get sign of the current action
+     sign='+' if num1==0 else '-' if num1==1 else 'x' if num1==2 else '/'
+     self.mem.append('{}{}{}={}  '.format(alpha1,sign,delta1,comb1))
+
+     # combine the combination of alpha & delta with gamma
+     for num2,comb2 in enumerate(Arith(comb1,gamma).combs()):
+      if num2:
+       del self.mem[1:]
+      # rearrange the order of comb1 & gamma
+      _comb1=comb1 if comb1>=gamma else gamma
+      _gamma=comb1 if _comb1==gamma else gamma
+
+      # get sign
+      sign='+' if not num2 else '-' if num2==1 else 'x' if num2==2 else '/'
+      self.mem.append('{}{}{}={}  '.format(_comb1,sign,_gamma,comb2))
+
+      #combine the combination of comb1 & gamma with theta
+      for num3,comb3 in enumerate(Arith(comb2,theta).combs()):
+       # rearrange the of order
+       _comb2=comb2 if comb2>=theta else theta
+       _theta=comb2 if _comb2==theta else theta
+
+       # get sign
+       sign='+' if not num3 else '-' if num3==1 else 'x' if num3==2 else '/'
+       self.mem.append('{}{}{}={}  '.format(_comb2,sign,_theta,comb3))
+       if comb3==24:
+        if len(self.mem)==3:
+         print('\n{}\n'.format(self.cleanDisplay(self.mem)))
+       del self.mem[-1]
+
+    # method 2
+    for num,comb in enumerate(Arith(alpha,delta).combs()):
+     del self.mem[:]
+     # rearrange the order
+     alpha1=alpha if alpha>=delta else delta
+     delta1=alpha if alpha1==delta else delta
+
+     # get sign of the current action
+     sign='+' if not num else '-' if num==1 else 'x' if num==2 else '/'
+     self.mem.append('{}{}{}={}  '.format(alpha1,sign,delta1,comb))
+
+     for num1,comb1 in enumerate(Arith(gamma,theta).combs()):
+      if num1:del self.mem[1:]
+      # rearrange the order
+      gamma1=gamma if gamma>=theta else theta
+      theta1=gamma if gamma1==theta else theta
+
+      # get sign of the current action
+      sign='+' if not num1 else '-' if num1==1 else 'x' if num1==2 else '/'
+      self.mem.append('{}{}{}={}  '.format(gamma1,sign,theta1,comb1))
+
+      # combine comb & comb1
+      for num2,comb2 in enumerate(Arith(comb,comb1).combs()):
+       # rearrange the order
+       phi=comb if comb>=comb1 else comb1
+       pi=comb if phi==comb1 else comb1
+
+       # get sign of the current action
+       sign='+' if not num2 else '-' if num2==1 else 'x' if num2==2 else '/'
+       self.mem.append('{}{}{}={}  '.format(phi,sign,pi,comb2))
+       if len(self.mem)==3:
+        if comb2==24:
+         print('\n{}\n'.format(self.cleanDisplay(self.mem)))
+       del self.mem[-1]
+
+ def cleanDisplay(self,lst):
+  return ''.join(lst).rstrip()
+
+class Numbers(object):
+ def __init__(self,lst):
+  self.lst = lst
+  self.num = []
+
+ def orginize(self):
+  for item in self.lst:
+   if item.isdigit():
+    self.num.append(item)
+   else:
+    yield eval(''.join(self.num))
+    del self.num[:]
+  if self.num:
+   yield eval(''.join(self.num))
+
+if __name__ == '__main__':
+ lst=list(Numbers(raw_input('Enter 4 number separated by commas: ')).orginize())
+ Solution(lst).solve()
